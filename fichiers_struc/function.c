ad040253066dea1ee713f23169611520de097529
AnimeruFR <erwan.corbin@etu.univ-lyon1.fr>
2022-12-07 09:13:22 +0100

    réglage de petit bug

--- fichiers_struc/function.c
+++ fichiers_struc/function.c
@@ -203,7 +203,7 @@ void **trier_clients_par_nom(entry **tab)
 		j = 0;
 		while(j < length_tab)
 		{
-			if(strcmp(tab[i]->lastname, tab[j]->lastname) >= 1 && i != j && i < j)
+			if(stricmp(tab[i]->lastname, tab[j]->lastname) >= 1 && i != j && i < j)
 			{
 					tmp=tab[i];
 					tab[i]=tab[j];
